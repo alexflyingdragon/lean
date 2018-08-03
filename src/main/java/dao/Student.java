19c74a9ee640be4c4448edc18ed13ba1432397b0
张翔龙 <xianglong.zhang@hand-china.com>
2018-08-03 10:12:57 +0800

    [feature] 新增Student类

---
+++ src/main/java/dao/Student.java
@@ -0,0 +1,16 @@
+package dao;
+
+/**
+ * Created by ALEX on 2018/8/3.
+ */
+public class Student {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
