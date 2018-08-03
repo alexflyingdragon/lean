95386bc55be2eee76a3ef8b33a82ca9e5277ec7f
张翔龙 <xianglong.zhang@hand-china.com>
2018-08-03 10:16:41 +0800

    [feature] 新增Student类

--- src/main/java/dao/Student.java
+++ src/main/java/dao/Student.java
@@ -6,11 +6,21 @@ package dao;
 public class Student {
     private String name;
 
+    private Long age;
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
     }
 }
